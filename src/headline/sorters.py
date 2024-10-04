e104b001293bf4bfc7e987ef0eb4625fe00da9c8
gazorpazorpius <edcussmusic@gmail.com>
2024-10-04 20:32:54 +0100

    feat: allow for custom indent level

--- src/headline/sorters.py
+++ src/headline/sorters.py
@@ -8,44 +8,45 @@ from ._logger import compress_logging_value
 logger = logging.getLogger()
 
 
-def sort_funcs_newspaper(funcs: List[FuncDef]) -> List[str]:
+def sort_funcs_newspaper(funcs: List[FuncDef], indent: int = 0) -> List[str]:
     for key, val in locals().items():
         logger.debug(f"{key} = {compress_logging_value(val)}")
     sorted_funcs = sorted(
         funcs, key=lambda f: (len(f.called), -len(f.calls), f.name.strip("_"))
     )
-    return [f.name for f in sorted_funcs if f.indent == 0]
+    return [f.name for f in sorted_funcs if f.indent == indent]
 
 
-def sort_funcs_calls(funcs: List[FuncDef]) -> List[str]:
+def sort_funcs_calls(funcs: List[FuncDef], indent: int = 0) -> List[str]:
     for key, val in locals().items():
         logger.debug(f"{key} = {compress_logging_value(val)}")
     sorted_funcs = sorted(
         funcs, key=lambda f: (-len(f.calls), f.name.strip("_"))
     )
-    return [f.name for f in sorted_funcs if f.indent == 0]
+    return [f.name for f in sorted_funcs if f.indent == indent]
 
 
-def sort_funcs_called(funcs: List[FuncDef]) -> List[str]:
+def sort_funcs_called(funcs: List[FuncDef], indent: int = 0) -> List[str]:
     for key, val in locals().items():
         logger.debug(f"{key} = {compress_logging_value(val)}")
     sorted_funcs = sorted(
         funcs, key=lambda f: (-len(f.called), f.name.strip("_"))
     )
-    return [f.name for f in sorted_funcs if f.indent == 0]
+    return [f.name for f in sorted_funcs if f.indent == indent]
 
 
-def sort_funcs_alphabetical(funcs: List[FuncDef]) -> List[str]:
+def sort_funcs_alphabetical(funcs: List[FuncDef], indent: int = 0) -> List[str]:
     for key, val in locals().items():
         logger.debug(f"{key} = {compress_logging_value(val)}")
     sorted_funcs = sorted(funcs, key=lambda f: f.name.strip("_"))
-    return [f.name for f in sorted_funcs if f.indent == 0]
+    return [f.name for f in sorted_funcs if f.indent == indent]
 
 
 def sort_funcs_alphabetical_inc_leading_underscores(
     funcs: List[FuncDef],
+    indent: int = 0
 ) -> List[str]:
     for key, val in locals().items():
         logger.debug(f"{key} = {compress_logging_value(val)}")
     sorted_funcs = sorted(funcs, key=lambda f: f.name)
-    return [f.name for f in sorted_funcs if f.indent == 0]
+    return [f.name for f in sorted_funcs if f.indent == indent]
